b24e0a58f1aca74e526813758e892049d5c8f8eb
Zach Evans <prescient.slime@gmail.com>
2026-03-31 21:03:16 -0500

    Simple enough... I get the feeling this exercise is going to make a comeback later.

---
+++ 1_20.rkt
@@ -0,0 +1,28 @@
+#lang sicp
+
+(define (square n)
+  (* n n))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+    ((divides? test-divisor n) test-divisor)
+    (else (find-divisor n (+ test-divisor 1)))
+    ))
+
+(newline)
+(display "smallest-divisor 199: ")
+(display (smallest-divisor 199))
+(newline)
+(display "smallest-divisor 1999: ")
+(display (smallest-divisor 1999))
+(newline)
+(display "smallest-divisor 19999: ")
+(display (smallest-divisor 19999))
+(newline)
+
