933009d5e4f7bcc1fba166aee29a561495c268d3
Zach Evans <prescient.slime@gmail.com>
2026-03-31 22:14:33 -0500

    1.21 and 1.22 complete. Not too much trouble.

---
+++ 1_21.rkt
@@ -0,0 +1,60 @@
+#lang sicp
+
+(define (square n)
+  (* n n))
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+    ((divides? test-divisor n) test-divisor)
+    (else (find-divisor n (+ test-divisor 1)))
+    ))
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime))
+  )
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (runtime) start-time))))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (search-for-prime flr n)
+  (cond ((= n 0) (newline))
+        ((even? flr)
+          (search-for-prime (+ flr 1) n))
+        (else (timed-prime-test flr)
+          (if (prime? flr)
+              (search-for-prime (+ flr 2) (- n 1))
+              (search-for-prime (+ flr 2) n)))))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(newline)
+(display "(search-for-prime 1000 3): ")
+(search-for-prime 1000 3)
+
+(newline)
+(display "(search-for-prime 10000 3): ")
+(search-for-prime 10000 3)
+
+(newline)
+(display "(search-for-prime 100000 3): ")
+(search-for-prime 100000 3)
+
+(newline)
+(display "(search-for-prime 1000000 3): ")
+(search-for-prime 1000000 3)
