dca475446fa2171cb12ca0a2a7ce72378b3036d5
Zach Evans <prescient.slime@gmail.com>
2026-04-13 22:17:22 -0500

    Rename to match actual exercise names

--- 1_21.rkt
+++ 1_21.rkt
@@ -2,6 +2,7 @@
 
 (define (square n)
   (* n n))
+
 (define (smallest-divisor n)
   (find-divisor n 2))
 
@@ -14,47 +15,14 @@
     (else (find-divisor n (+ test-divisor 1)))
     ))
 
-(define (timed-prime-test n)
-  (newline)
-  (display n)
-  (start-prime-test n (runtime))
-  )
-
-(define (start-prime-test n start-time)
-  (if (prime? n)
-      (report-prime (- (runtime) start-time))))
-
-(define (report-prime elapsed-time)
-  (display " *** ")
-  (display elapsed-time))
-
-(define (even? n)
-  (= (remainder n 2) 0))
-
-(define (search-for-prime flr n)
-  (cond ((= n 0) (newline))
-        ((even? flr)
-          (search-for-prime (+ flr 1) n))
-        (else (timed-prime-test flr)
-          (if (prime? flr)
-              (search-for-prime (+ flr 2) (- n 1))
-              (search-for-prime (+ flr 2) n)))))
-
-(define (prime? n)
-  (= n (smallest-divisor n)))
-
 (newline)
-(display "(search-for-prime 1000 3): ")
-(search-for-prime 1000 3)
-
+(display "smallest-divisor 199: ")
+(display (smallest-divisor 199))
 (newline)
-(display "(search-for-prime 10000 3): ")
-(search-for-prime 10000 3)
-
+(display "smallest-divisor 1999: ")
+(display (smallest-divisor 1999))
 (newline)
-(display "(search-for-prime 100000 3): ")
-(search-for-prime 100000 3)
-
+(display "smallest-divisor 19999: ")
+(display (smallest-divisor 19999))
 (newline)
-(display "(search-for-prime 1000000 3): ")
-(search-for-prime 1000000 3)
+
