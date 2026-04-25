3565fc3c87c778e7bc091e74d81cde82694fa38b
Zach Evans <prescient.slime@gmail.com>
2026-04-25 12:43:17 -0500

    1.35 completed

---
+++ 1_35.rkt
@@ -0,0 +1,30 @@
+#lang sicp
+
+(define tolerance 0.00001)
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next(try next))))
+  (try first-guess))
+
+(define (sqrt x)
+  (fixed-point (lambda (y) (average y (/ x y)))
+               1.0))
+
+(define phi (/ (+ 1 (sqrt 5)) 2))
+
+(newline)
+(display "Fixed point of 1 + 1/x: ")
+(display (fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0))
+(newline)
+(display "Phi via fixed-point: ")
+(display phi)
+(newline)
+
